6a3573f10122024f66e867d06839c2a85f9dbdfc
uma-116 <yumasato116@gmail.com>
2020-05-08 16:53:42 +0900

    create login-view and user-registration-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :users, only: [:show] do
+
+    collection do
+      get "log_in"
+      get "new_user"
+    end
+  end 
 end
