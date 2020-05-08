d34bf656d5d6d4392e6bf839fa05ab1902592f88
Masaaki-Kimura06 <kimu06.0412@gmail>
2020-05-08 13:57:38 +0900

    create_view_item-purchase-confirmation-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #購入機能実装時にitemsに対してネスト設定を行う（item_id情報を受け取るため）
+  resources :purchases, only: [:index]
+
 end
