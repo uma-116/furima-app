6357b7552748028a56567f4d2ffc3f5e15bb10e6
mamamamaeda <mae--d--tomoaki@hotmail.co.jp>
2020-05-10 15:31:55 +0900

    itemコントローラーに追記
    newアクション
    createアクション
    ストロングパラメーターなど

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+     #とりあえずリダイレクト先をトップページに設定
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    # :user_id, :category_idの２つはテスト用なので後で削除
+    params.require(:item).permit(:user_id, :category_id, :name, :detail, :condition, :postage, :ship_from, :ship_date, :price)
+  end
+
+
 end
