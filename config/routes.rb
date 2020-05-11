aac7b63482b413159c78a468fb3f794679e39a4a
Uya16 <ymogashi@gmail.com>
2020-05-11 11:48:29 +0900

    fixup!create view item-details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :item_details, only: [:index, :edit, :update, :destroy]
+
+    
+  #購入機能実装時にitemsに対してネスト設定を行う（item_id情報を受け取るため）
+  resources :purchases, only: [:index]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :users, only: [:show] do
+
+    collection do
+      get "log_in"
+      get "new_user"
+    end
+  end
 end
