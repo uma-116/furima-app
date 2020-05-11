4ac6d320ae8d82ac155b2cf214667090f9de45e5
uma-116 <yumasato116@gmail.com>
2020-05-11 16:30:48 +0900

    create devise controllers and changed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
   root 'items#index'
   resources :item_details, only: [:index, :edit, :update, :destroy]
 
   #購入機能実装時にitemsに対してネスト設定を行う（item_id情報を受け取るため）
   resources :purchases, only: [:index]
 
-  resources :users, only: [:show] do
-
-    collection do
-      get "log_in"
-      get "new_user"
-    end
+  resources :users, only: [:show] 
+  
   end 
 end
