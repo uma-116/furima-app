adccdd6aa8d635afb4cc70fbe110fac2a7edef8e
uma-116 <yumasato116@gmail.com>
2020-05-12 18:57:07 +0900

    edit address model and migration file

--- db/migrate/20200512094620_create_addresses.rb
+++ db/migrate/20200512094620_create_addresses.rb
@@ -1,7 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-
+      t.references :user       , null: false, foreign_key: true
+      t.integer    :postal_code, null: false
+      t.string     :prefecture , null: false
+      t.string     :city       , null: false
+      t.string     :address     , null: false
+      t.string     :apartment_name
       t.timestamps
     end
   end
