e01bd98dac839bffe932aa128cc354ee3669eddc
mamamamaeda <mae--d--tomoaki@hotmail.co.jp>
2020-05-14 23:32:23 +0900

    itemへのバリデーション
    子テーブルのimagesへのバリデーションと
    本テーブルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  validates_associated :images
+  validates :images, presence: true
+
+  validates :name, :detail, :condition, :price, :postage, :ship_from, :ship_date, presence: true
+
 end
