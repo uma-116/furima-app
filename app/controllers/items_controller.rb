6daf58d6ca41472fe0e064a92d19e4e92294b93e
mamamamaeda <mae--d--tomoaki@hotmail.co.jp>
2020-05-17 13:07:28 +0900

    bindingの消去

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,11 +12,10 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       render :new
-      binding.pry
     end
   end
 
