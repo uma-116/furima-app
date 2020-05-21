eec55e1bfe2e6778515ebfd421066b01560dc960
uma-116 <yumasato116@gmail.com>
2020-05-21 18:18:28 +0900

    Update searches_controller_spec.rb

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
   describe Items::SearchesController do
-    it "@itemsに正しい値が入っていること" do
-    end
+    describe 'GET #index' do
+      it "@itemsに正しい値が入っていること" do
+        get :index
+        expect(assigns(:@items)).to match(@items)
+      end
 
-    it "index.html.hamlに遷移すること" do
+      it "index.html.hamlに遷移すること" do
+        get :index
+        expect(response).to render_template :index
+      end
+      
     end
-  end
 
 end
