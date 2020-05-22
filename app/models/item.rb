18ddfe17e91fd8c56423d2be1f9543e109e58179
uma-116 <yumasato116@gmail.com>
2020-05-22 10:27:16 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,6 @@ class Item < ApplicationRecord
   # items table association
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
-
   has_many :comments
   belongs_to :category
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
