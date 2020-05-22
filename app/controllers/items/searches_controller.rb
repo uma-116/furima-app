41bfe9a41b56bb600d65ca932198980446d17188
uma-116 <yumasato116@gmail.com>
2020-05-22 10:31:21 +0900

    Update searches_controller.rb

--- app/controllers/items/searches_controller.rb
+++ app/controllers/items/searches_controller.rb
@@ -1,6 +1,6 @@
 class Items::SearchesController < ApplicationController
   def index
-    @items = Item.search(params[:keyword])
+    @items = Item.search(params[:keyword]).order('created_at DESC').where("buyer_id is NULL")
     @count = @items.count
   end
 end
