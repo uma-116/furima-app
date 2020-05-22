89bf1d9c666affa5e88de9a1079366b52264a123
uma-116 <yumasato116@gmail.com>
2020-05-22 22:24:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,9 @@ set :keep_releases, 5
 
 set :linked_files, %w{ config/master.key }
 
+#下１行試しに追加
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
