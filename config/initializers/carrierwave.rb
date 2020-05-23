86b92eb60f766fdb3c97291fcf457be2ff733875
uma-116 <yumasato116@gmail.com>
2020-05-23 18:41:52 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
 
-  config.fog_directory  = 'furima-app74d'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima-app74d'
+    config.fog_directory  = 'furima-app74d'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima-app74d'
+  end
 end
