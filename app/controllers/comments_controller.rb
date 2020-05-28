5aa30a96715d959901f3ab2050ac0e7f1eda56f0
Uya16 <ymogashi@gmail.com>
2020-05-28 14:57:47 +0900

    fixup!controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    respond_to do |format|
-      format.html {redirect_to item_path(@comment.item_id)}
-      format.json
+    if @comment.save
+      respond_to do |format|
+        format.html {redirect_to item_path(@comment.item_id)}
+        format.json
+      end
+    else
+      redirect_to item_path(@comment.item_id)
     end
   end
 
